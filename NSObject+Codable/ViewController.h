58f9679800cc12f9b1217c6d6a31c111b911d745
Logan Wright <logan.william.wright@gmail.com>
2015-02-20 14:09:01 -0500

    Initial Commit

---
+++ NSObject+Codable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSObject+Codable
+//
+//  Created by Logan Wright on 2/20/15.
+//  Copyright (c) 2015 LowriDevs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
